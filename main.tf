3ed670857cb9e52366f326ca7edba3a60719ac21
Mounir <mounir.gazanayi@polymtlus.ca>
2024-04-13 22:31:22 -0400

    update

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "azurerm_network_interface_security_group_association" "nsg" {
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "strcr380xvlume25"
+  name                     = "strcr380volume"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
